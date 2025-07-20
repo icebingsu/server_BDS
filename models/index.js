706132f3d53becf1b7abea1ed865126124e7ed5f
icebingsu <botchat549@gmail.com>
2025-07-20 19:40:46 +0700

    config db

--- models/index.js
+++ models/index.js
@@ -3,28 +3,14 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-
 const db = {};
 
-let sequelize;
-
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
-}
+// import instance sequelize từ database.js
+const sequelize = require('../config/database'); 
 
 fs.readdirSync(__dirname)
-  .filter((file) => {
+  .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
       file !== basename &&
@@ -32,20 +18,18 @@ fs.readdirSync(__dirname)
       !file.endsWith('.test.js')
     );
   })
-  .forEach((file) => {
+  .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
-Object.keys(db).forEach((modelName) => {
-  if (typeof db[modelName].associate === 'function') {
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
-
 
 module.exports = db;
